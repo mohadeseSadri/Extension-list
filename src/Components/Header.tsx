4fa3c1440d68e31ce76064b0db4f17c60ca1b6bb
Mohadese <mohadesehsadr788@gmail.com>
2025-09-02 10:05:38 +0330

    Building header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import logoImg from "../assets/images/logo.svg";
+import sunImg from "../assets/images/icon-sun.svg";
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <header className="flex justify-center">
+        <div className="flex justify-between m-5 w-[80%] bg-[#1F2535] rounded-xl">
+          <div>
+            <img src={logoImg} alt="logo" className="p-2" />
+          </div>
+
+          <div>
+            <button>
+                <img src={sunImg} alt="sun" className="p-2 m-2 bg-[#2E3447] rounded-xl" />
+            </button>
+          </div>
+        </div>{" "}
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
