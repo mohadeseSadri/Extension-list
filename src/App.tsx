d7867ab110cdfa55c53df981e430051502257756
Mohadese <mohadesehsadr788@gmail.com>
2025-09-14 13:01:08 +0330

    building category

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
-
+import Category from './Components/Category';
 function App() {
   
 
   return (
     <>
       <Header />
+      <Category />
     </>
   )
 }
---
+++ src/Components/Category.tsx
@@ -0,0 +1,29 @@
+function Category() {
+  return (
+    <>
+      <section className="flex justify-center">
+        <div className="m-5 flex w-[80%] justify-between">
+          <header>
+            <h2 className="text-2xl font-bold">Extensions List</h2>
+          </header>
+
+          <ul>
+            <li>
+              <button className="mx-1 rounded-2xl bg-[#1F2535] px-3 py-1">
+                All
+              </button>
+              <button className="mx-1 rounded-2xl bg-[#1F2535] px-3 py-1">
+                Active
+              </button>
+              <button className="mx-1 rounded-2xl bg-[#1F2535] px-3 py-1">
+                Inactive
+              </button>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Category;
