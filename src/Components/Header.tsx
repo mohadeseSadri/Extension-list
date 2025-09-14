ef4cef9a117f13852bd7b916fc43354f29ab55cb
Mohadese <mohadesehsadr788@gmail.com>
2025-09-14 13:00:21 +0330

    Set semantic HTML

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,16 +4,19 @@ function Header() {
   return (
     <>
       <header className="flex justify-center">
-        <div className="flex justify-between m-5 w-[80%] bg-[#1F2535] rounded-xl">
-          <div>
-            <img src={logoImg} alt="logo" className="p-2" />
-          </div>
+        <div className="m-5 flex w-[80%] justify-between rounded-xl bg-[#1F2535]">
+          <a>
+            <img src={logoImg} alt="App name logo" className="p-2" />
+          </a>
 
-          <div>
-            <button>
-                <img src={sunImg} alt="sun" className="p-2 m-2 bg-[#2E3447] rounded-xl" />
-            </button>
-          </div>
+          <button>
+            <img
+              src={sunImg}
+              alt="sun"
+              className="m-2 rounded-xl bg-[#2E3447] p-2"
+              aria-label="Toggle theme"
+            />
+          </button>
         </div>{" "}
       </header>
     </>
